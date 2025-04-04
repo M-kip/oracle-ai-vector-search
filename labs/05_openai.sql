b4084f2d1095a191fc35e0ce7ca75d87c3782488
Angelo Stramieri <angelo.stramieri@gmail.com>
2025-04-04 13:57:32 +0200

    Update docs

--- labs/05_openai.sql
+++ labs/05_openai.sql
@@ -1,6 +1,15 @@
+-- Generates text for a prompt (input string) 
 select dbms_vector_chain.utl_to_generate_text(
     'What is Oracle?',
     json('{"provider": "openai", 
            "credential_name": "OPENAI", 
            "url": "https://api.openai.com/v1/chat/completions", 
-           "model": "gpt-4o-mini"}'));
+           "model": "gpt-4o"}'));
+           
+-- Extracts a summary from documents
+select dbms_vector_chain.utl_to_summary(
+    'What is Oracle?',
+    json('{"provider": "openai", 
+           "credential_name": "OPENAI", 
+           "url": "https://api.openai.com/v1/chat/completions", 
+           "model": "gpt-4o"}'));
